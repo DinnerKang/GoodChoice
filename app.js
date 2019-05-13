2aefbee3a003acc5170bfceeaf0c826d4e209c70
DinnerKang <jenhyuk0318@gmail.com>
2019-05-13 15:35:58 +0900

    server 초기 셋팅

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const port = 5000;
+app.set('port', port);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cors());
+
+// 요청 테스트
+app.get('/', (req, res) => {
+    res.send('Hello gram');
+});
+
+
+app.listen(port, () => {
+    console.log('서버 시작');
+});
