a4a84dec466cccaffd3780ec07c051d4fb8a1b1c
DinnerKang <jenhyuk0318@gmail.com>
2019-05-13 23:47:50 +0900

    client 기본 셋팅 완료

---
+++ client/src/components/Main.js
@@ -0,0 +1,23 @@
+import React, { Component, Fragment }from 'react';
+
+import './Main.css';
+
+class Main extends Component{
+    constructor(props){
+        super(props);
+        this.state ={
+
+        };
+    }
+    render(){
+        return(
+            <Fragment>
+                <section>
+                    <div>Hi</div>
+                </section>
+            </Fragment>
+        )
+    }
+}
+
+export default Main;
---
+++ client/src/root/App.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+import Main from '../components/Main';
+
+import './App.css';
+
+class App extends Component{
+
+  render(){
+    return(
+      <Fragment>
+        <Main></Main>
+      </Fragment>
+    )
+  }
+}
+
+export default App;
